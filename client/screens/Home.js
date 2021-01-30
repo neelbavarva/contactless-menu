05b0a81bacaa044d9560094c75a816c9c97135c1
neelbavarva <neelbavarva5@gmail.com>
2021-01-30 19:33:46 +0530

    nabar changed everywhere

--- client/screens/Home.js
+++ client/screens/Home.js
@@ -6,11 +6,77 @@ export default function Home() {
     return (
         <View>
 
+            
+
+            {/* Navbar */}
+
+            <View style={{ flex: 1, justifyContent: 'center', padding: 24, marginTop: 30, marginBottom: 20}}>
+                    <View style={{ flexDirection: 'row', height: 55, backgroundColor: "#ECECEC", borderRadius: 8 }}>
+                        
+                        <TouchableOpacity
+                            style={{ 
+                                flex: 1,
+                                backgroundColor: null, 
+                                borderRadius: 6,
+                            }}
+                            
+                        >
+                            <View style={{ flex: 1, flexDirection: 'row', alignItems: 'center',marginRight: 20, justifyContent: 'center' }}>
+                            
+                            </View>
+                        </TouchableOpacity>
+
+                        <TouchableOpacity
+                            style={{ 
+                                flex: 1,
+                                backgroundColor:null, 
+                                margin: 5,
+                                borderRadius: 8,
+                            }}
+                            // onPress={() => NavbarButtonHandler("chart")}
+                        >
+                            <View
+                                style={{
+                                    flex: 1,
+                                    flexDirection: 'row',
+                                    alignItems: 'center',
+                                    justifyContent: 'center'
+                                }}
+                            >
+                                <Text style={{ marginLeft: 0, fontSize: 16, fontFamily: 'GothamMedium', lineHeight: 22, color: "black" }}>App ka Name</Text>
+                            </View>
+                        </TouchableOpacity>
+
+                        <TouchableOpacity
+                            style={{ 
+                                flex: 1,
+                                backgroundColor: null, 
+                                margin: 5,
+                                borderRadius: 8,
+                            }}
+                            // onPress={() => NavbarButtonHandler("add")}
+                        >
+                            <View
+                                style={{
+                                    flex: 1,
+                                    flexDirection: 'row',
+                                    alignItems: 'center',
+                                    justifyContent: 'center',
+                                }}
+                            >
+                               
+                            </View>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+
+
             <Image style={{
                 width: 250,
                 height: 250,
                 alignSelf: "center",
-                marginTop: 100
+                marginTop: 30, 
+                marginBottom: 30
             }} source={require('../assets/qr.png')}  />
 
             <TouchableOpacity 
--- client/screens/Menu.js
+++ client/screens/Menu.js
@@ -16,6 +16,76 @@ export default function Menu() {
 
     return (
         <View >
+
+            {/* Navbar */}
+
+            <View style={{ flex: 1, justifyContent: 'center', padding: 24, marginTop: 30, marginBottom: 20}}>
+                    <View style={{ flexDirection: 'row', height: 55, backgroundColor: "#ECECEC", borderRadius: 8 }}>
+                        
+                        <TouchableOpacity
+                            style={{ 
+                                flex: 1,
+                                backgroundColor: null, 
+                                borderRadius: 6,
+                            }}
+                            
+                        >
+                            <View style={{ flex: 1, flexDirection: 'row', alignItems: 'center',marginRight: 20, justifyContent: 'center' }}>
+                            <Image 
+                                style={{
+                                    width: 20, 
+                                    height: 20, 
+                                }}
+                                tintColor= "#1D256E"
+                                source={require('../assets/icons/back_arrow_icon.png')} />
+                            </View>
+                        </TouchableOpacity>
+
+                        <TouchableOpacity
+                            style={{ 
+                                flex: 1,
+                                backgroundColor:null, 
+                                margin: 5,
+                                borderRadius: 8,
+                            }}
+                            // onPress={() => NavbarButtonHandler("chart")}
+                        >
+                            <View
+                                style={{
+                                    flex: 1,
+                                    flexDirection: 'row',
+                                    alignItems: 'center',
+                                    justifyContent: 'center'
+                                }}
+                            >
+                                <Text style={{ marginLeft: 0, fontSize: 16, fontFamily: 'GothamMedium', lineHeight: 22, color: "black" }}>Menu</Text>
+                            </View>
+                        </TouchableOpacity>
+
+                        <TouchableOpacity
+                            style={{ 
+                                flex: 1,
+                                backgroundColor: null, 
+                                margin: 5,
+                                borderRadius: 8,
+                            }}
+                            // onPress={() => NavbarButtonHandler("add")}
+                        >
+                            <View
+                                style={{
+                                    flex: 1,
+                                    flexDirection: 'row',
+                                    alignItems: 'center',
+                                    justifyContent: 'center',
+                                }}
+                            >
+                               <Text style={{ marginLeft: 0, fontSize: 16, fontFamily: 'GothamMedium', lineHeight: 22, color: "black" }}>View Cart</Text>
+                            </View>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+
+
             {menu_items.map(item => (
                 <View key={item.name}
                     style={{
