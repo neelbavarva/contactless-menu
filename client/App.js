ccf0ee37f7e89240604e9667d0acd49036738948
neel-thakker <neelthakker2001@gmail.com>
2021-01-30 19:27:54 +0530

    Changes in navbar

--- client/App.js
+++ client/App.js
@@ -11,106 +11,34 @@ import AppLoading from 'expo-app-loading';
 
 export default function App() {
 
-  const [viewMode, setviewMode] = useState("scan");
+  	const [viewMode, setviewMode] = useState("scan");
 
-  let [fontsLoaded] = useFonts({
-		'GothamBlack': require('./assets/fonts/Gotham-Black.otf'),
-		'GothamBold': require('./assets/fonts/Gotham-Bold.otf'),
-		'GothamMedium': require('./assets/fonts/GothamMedium.ttf'),
-		'GothamLight': require('./assets/fonts/GothamLight.ttf'),
-		'GothamThin': require('./assets/fonts/Gotham-Thin.otf'),
-		'GothamBoldItalic': require('./assets/fonts/GothamBoldItalic.ttf'),
-		'GothamBookItalic': require('./assets/fonts/Gotham-BookItalic.otf')
+	let [fontsLoaded] = useFonts({
+			'GothamBlack': require('./assets/fonts/Gotham-Black.otf'),
+			'GothamBold': require('./assets/fonts/Gotham-Bold.otf'),
+			'GothamMedium': require('./assets/fonts/GothamMedium.ttf'),
+			'GothamLight': require('./assets/fonts/GothamLight.ttf'),
+			'GothamThin': require('./assets/fonts/Gotham-Thin.otf'),
+			'GothamBoldItalic': require('./assets/fonts/GothamBoldItalic.ttf'),
+			'GothamBookItalic': require('./assets/fonts/Gotham-BookItalic.otf')
 	});
 
 	if (!fontsLoaded) {
 		return <AppLoading />;
-  }
+	}
   
-  return (
-    <ScrollView>
-      <View style={{ flex: 1, justifyContent: 'center', padding: 24, marginTop: 30, marginBottom: 20}}>
-                <View style={{ flexDirection: 'row', height: 55, backgroundColor: "#ECECEC", borderRadius: 8 }}>
-                        
-                    <TouchableOpacity
-                        style={{ 
-                            flex: 1,
-                            backgroundColor: null, 
-                            borderRadius: 6,
-                        }}
-                        onPress={() =>setviewMode("scan")}
-                    >
-                        <View
-                            style={{
-                                flex: 1,
-                                flexDirection: 'row',
-                                alignItems: 'center',
-                                justifyContent: 'center',
-                                backgroundColor: viewMode == "scan" ? "gray" : null,
-                                borderRadius: 10
-                            }}
-                        >
-                            <Text style={{ marginLeft: 0, fontSize: 16, fontFamily: 'GothamMedium', lineHeight: 22, color: "black" }}>Scan QR</Text>
-                        </View>
-                    </TouchableOpacity>
-
-                    <TouchableOpacity
-                        style={{ 
-                            flex: 1,
-                            backgroundColor:null, 
-                            margin: 5,
-                            borderRadius: 8,
-                            backgroundColor: viewMode == "menu" ? "gray" : null,
-                            borderRadius: 10
-                        }}
-                        onPress={() =>setviewMode("menu")}
-                    >
-                        <View
-                            style={{
-                                flex: 1,
-                                flexDirection: 'row',
-                                alignItems: 'center',
-                                justifyContent: 'center'
-                            }}
-                        >
-                            <Text style={{ marginLeft: 0, fontSize: 16, fontFamily: 'GothamMedium', lineHeight: 22, color: "black" }}>Menu</Text>
-                        </View>
-                    </TouchableOpacity>
-
-                    <TouchableOpacity
-                        style={{ 
-                            flex: 1,
-                            backgroundColor: null, 
-                            margin: 5,
-                            borderRadius: 8,
-                            backgroundColor: viewMode == "cart" ? "gray" : null,
-                            borderRadius: 10
-                        }}
-                        onPress={() =>setviewMode("cart")}
-                    >
-                        <View
-                            style={{
-                                flex: 1,
-                                flexDirection: 'row',
-                                alignItems: 'center',
-                                justifyContent: 'center',
-                            }}
-                        >
-                            <Text style={{ marginLeft: 0, fontSize: 16, fontFamily: 'GothamMedium', lineHeight: 22, color: "black" }}>Cart</Text>   
-                        </View>
-                    </TouchableOpacity>
-                </View>
-            </View>
-            {viewMode == "scan" && <Home />}
-            {viewMode == "menu" && <Menu />}
-            {viewMode == "cart" && <Cart />}
-    </ScrollView>
-  );
+	return (
+		<ScrollView>
+			{viewMode == "scan" && <Home />}
+			{viewMode == "menu" && <Menu />}
+			{viewMode == "cart" && <Cart />}
+		</ScrollView>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+	},
 });
